437b11277e56c4c065069a57fcf50f47d6b47311
sebaactis <sebaactis@gmail.com>
2025-01-10 14:48:18 -0300

    fix tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+import { nextui } from '@nextui-org/theme'
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [
+					'var(--font-sans)'
+				],
+				mono: [
+					'var(--font-mono)'
+				]
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			colors: {}
+		}
+	},
+	darkMode: ["class", 'class'],
+	plugins: [nextui(), require("tailwindcss-animate")],
+} satisfies Config
