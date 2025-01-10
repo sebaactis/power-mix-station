51109321c89dd11044c7673832dd3dc787a7e410
sebaactis <sebaactis@gmail.com>
2025-01-10 14:36:10 -0300

    fallback fonts

--- utils/fonts.js
+++ utils/fonts.js
@@ -3,5 +3,6 @@ import { Mona_Sans } from 'next/font/google'
 export const monaSans = Mona_Sans({
     subsets: ['latin'],
     weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
-    style: ['italic', 'normal']
+    style: ['italic', 'normal'],
+    fallback: ['Comic Neue', 'Arial', 'sans-serif'],
 })
