aff7173e611ae2be747a4d5e7ccd6988d23f07ab
sebaactis <sebaactis@gmail.com>
2025-01-10 14:43:21 -0300

    .env-local

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,30 @@
-import { nextui } from '@nextui-org/theme'
-import tailwindcssAnimate from 'tailwindcss-animate';
+import {nextui} from '@nextui-org/theme'
 
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: [
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: [
-					'var(--font-sans)'
-				],
-				mono: [
-					'var(--font-mono)'
-				]
-			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
-			},
-			colors: {}
-		}
-	},
-	darkMode: ["class", 'class'],
-	plugins: [nextui(), tailwindcssAnimate()],
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+  	extend: {
+  		fontFamily: {
+  			sans: [
+  				'var(--font-sans)'
+  			],
+  			mono: [
+  				'var(--font-mono)'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  darkMode: ["class", 'class'],
+  plugins: [nextui(), require("tailwindcss-animate")],
 }
